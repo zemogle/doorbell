153755a8f1ce7214b5da3d4825492151d13e7ece
Edward Gomez <edward@gomez.me.uk>
2021-01-20 08:25:37 +0000

    Better colours

--- alert-blynkt.py
+++ alert-blynkt.py
@@ -22,10 +22,11 @@ def parse_response(data):
     else:
         return False
 
-def blink_blinkt():
+def blink_blinkt(colours):
+    blinkt.set_brightness(0.5)
     for i in range(3):
         for j in range(blinkt.NUM_PIXELS):
-            blinkt.set_pixel(j, 255, 0, 0)
+            blinkt.set_pixel(j, colours[0], colours[1], colours[2])
 
         blinkt.show()
         time.sleep(0.1)
@@ -50,10 +51,17 @@ def main():
             status = parse_response(resp.json())
         elif resp.status_code != 304:
             # back off if the server is throwing errors
+            blink_blinkt(colours=[255,0,0])
             time.sleep(60)
             continue
-        if status and (datetime.now() - status) < timedelta(minutes=10):
-            blink_blinkt()
+        if status:
+            timesince = datetime.now() - status
+            if timesince < timedelta(minutes=1):
+                blink_blinkt(colours=[0,0,255])
+            elif timesince >= timedelta(minutes=1) and timesince < timedelta(days=1):
+                blinkt.set_pixel(7, 255, 0, 0)
+                blinkt.set_brightness(0.5)
+                blinkt.show()
         time.sleep(0.1)
 
 if __name__ == "__main__":
